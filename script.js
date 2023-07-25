1c7bff5663000c84cbc3aea4f7c09d6ccab9683d
athulpm <97athulpm@gmail.com>
2023-07-25 16:27:11 +0530

    basic dom manipulation

--- script.js
+++ script.js
@@ -1,26 +1,286 @@
-async function fetchData() {
-  const options = {
-    method: "GET",
-    url: "https://concerts-artists-events-tracker.p.rapidapi.com/venue/past",
-    params: {
-      name: "Hollywood bowl",
-      minDate: "2022-01-25",
-      maxDate: "2023-01-30",
-      page: "1",
+const data = {
+  layout: "5",
+  type: "MUSIC",
+  key: "40333609",
+  title: "River Flows In You",
+  subtitle: "Yiruma",
+  images: {
+    background:
+      "https://is4-ssl.mzstatic.com/image/thumb/Features115/v4/3f/84/4e/3f844e97-8c23-b64d-87f1-d536e99f80e9/mzl.rsxdpqqq.jpg/800x800cc.jpg",
+    coverart:
+      "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/b5/ae/26/b5ae2651-542a-9ee9-be4c-278791e2918f/mzi.ncrxyndi.jpg/400x400cc.jpg",
+    coverarthq:
+      "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/b5/ae/26/b5ae2651-542a-9ee9-be4c-278791e2918f/mzi.ncrxyndi.jpg/400x400cc.jpg",
+    joecolor: "b:302b35p:ffffffs:efebf2t:d5d4d6q:c9c5cc",
+  },
+  share: {
+    subject: "River Flows In You - Yiruma",
+    text: "I used Shazam to discover River Flows In You by Yiruma.",
+    href: "https://www.shazam.com/track/40333609/river-flows-in-you",
+    image:
+      "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/b5/ae/26/b5ae2651-542a-9ee9-be4c-278791e2918f/mzi.ncrxyndi.jpg/400x400cc.jpg",
+    twitter: "I used @Shazam to discover River Flows In You by Yiruma.",
+    html: "https://www.shazam.com/snippets/email-share/40333609?lang=en-US&country=US",
+    avatar:
+      "https://is4-ssl.mzstatic.com/image/thumb/Features115/v4/3f/84/4e/3f844e97-8c23-b64d-87f1-d536e99f80e9/mzl.rsxdpqqq.jpg/800x800cc.jpg",
+    snapchat: "https://www.shazam.com/partner/sc/track/40333609",
+  },
+  hub: {
+    type: "APPLEMUSIC",
+    image:
+      "https://images.shazam.com/static/icons/hub/ios/v5/applemusic_{scalefactor}.png",
+    actions: [
+      {
+        name: "apple",
+        type: "applemusicplay",
+        id: "485353578",
+      },
+      {
+        name: "apple",
+        type: "uri",
+        uri: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/cb/24/ac/cb24ac75-128c-f817-7d81-7fbffbdaafe3/mzaf_1522770279362005862.plus.aac.ep.m4a",
+      },
+    ],
+    options: [
+      {
+        caption: "OPEN",
+        actions: [
+          {
+            name: "hub:applemusic:deeplink",
+            type: "applemusicopen",
+            uri: "https://music.apple.com/us/album/river-flows-in-you/485353575?i=485353578&mttnagencyid=s2n&mttnsiteid=125115&mttn3pid=Apple-Shazam&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios",
+          },
+          {
+            name: "hub:applemusic:deeplink",
+            type: "uri",
+            uri: "https://music.apple.com/us/album/river-flows-in-you/485353575?i=485353578&mttnagencyid=s2n&mttnsiteid=125115&mttn3pid=Apple-Shazam&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios",
+          },
+        ],
+        beacondata: {
+          type: "open",
+          providername: "applemusic",
+        },
+        image:
+          "https://images.shazam.com/static/icons/hub/ios/v5/overflow-open-option_{scalefactor}.png",
+        type: "open",
+        listcaption: "Open in Apple Music",
+        overflowimage:
+          "https://images.shazam.com/static/icons/hub/ios/v5/applemusic-overflow_{scalefactor}.png",
+        colouroverflowimage: false,
+        providername: "applemusic",
+      },
+      {
+        caption: "BUY",
+        actions: [
+          {
+            type: "uri",
+            uri: "https://itunes.apple.com/us/album/river-flows-in-you/485353575?i=485353578&mttnagencyid=s2n&mttnsiteid=125115&mttn3pid=Apple-Shazam&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=itunes&itsct=Shazam_ios",
+          },
+        ],
+        beacondata: {
+          type: "buy",
+          providername: "itunes",
+        },
+        image:
+          "https://images.shazam.com/static/icons/hub/ios/v5/itunes-overflow-buy_{scalefactor}.png",
+        type: "buy",
+        listcaption: "Buy on iTunes",
+        overflowimage:
+          "https://images.shazam.com/static/icons/hub/ios/v5/itunes-overflow-buy_{scalefactor}.png",
+        colouroverflowimage: false,
+        providername: "itunes",
+      },
+    ],
+    providers: [
+      {
+        caption: "Open in Spotify",
+        images: {
+          overflow:
+            "https://images.shazam.com/static/icons/hub/ios/v5/spotify-overflow_{scalefactor}.png",
+          default:
+            "https://images.shazam.com/static/icons/hub/ios/v5/spotify_{scalefactor}.png",
+        },
+        actions: [
+          {
+            name: "hub:spotify:searchdeeplink",
+            type: "uri",
+            uri: "spotify:search:River%20Flows%20In%20You%20Yiruma",
+          },
+        ],
+        type: "SPOTIFY",
+      },
+      {
+        caption: "Open in Deezer",
+        images: {
+          overflow:
+            "https://images.shazam.com/static/icons/hub/ios/v5/deezer-overflow_{scalefactor}.png",
+          default:
+            "https://images.shazam.com/static/icons/hub/ios/v5/deezer_{scalefactor}.png",
+        },
+        actions: [
+          {
+            name: "hub:deezer:searchdeeplink",
+            type: "uri",
+            uri: "deezer-query://www.deezer.com/play?query=%7Btrack%3A%27River+Flows+In+You%27%20artist%3A%27Yiruma%27%7D",
+          },
+        ],
+        type: "DEEZER",
+      },
+    ],
+    explicit: false,
+    displayname: "APPLE MUSIC",
+  },
+  url: "https://www.shazam.com/track/40333609/river-flows-in-you",
+  artists: [
+    {
+      id: "40008598",
+      adamid: "73406786",
     },
-    headers: {
-      "X-RapidAPI-Key": "81a5c3c5femsh66ea56df1782fbdp1a2c71jsnd77886b8fef8",
-      "X-RapidAPI-Host": "concerts-artists-events-tracker.p.rapidapi.com",
+  ],
+  isrc: "KRA031103194",
+  genres: {
+    primary: "Instrumental",
+  },
+  urlparams: {
+    "{tracktitle}": "River+Flows+In+You",
+    "{trackartist}": "Yiruma",
+  },
+  myshazam: {
+    apple: {
+      actions: [
+        {
+          name: "myshazam:apple",
+          type: "uri",
+          uri: "https://music.apple.com/us/album/river-flows-in-you/485353575?i=485353578&mttnagencyid=s2n&mttnsiteid=125115&mttn3pid=Apple-Shazam&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios",
+        },
+      ],
     },
-  };
+  },
+  albumadamid: "485353575",
+  sections: [
+    {
+      type: "SONG",
+      metapages: [
+        {
+          image:
+            "https://is4-ssl.mzstatic.com/image/thumb/Features115/v4/3f/84/4e/3f844e97-8c23-b64d-87f1-d536e99f80e9/mzl.rsxdpqqq.jpg/800x800cc.jpg",
+          caption: "Yiruma",
+        },
+        {
+          image:
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/b5/ae/26/b5ae2651-542a-9ee9-be4c-278791e2918f/mzi.ncrxyndi.jpg/400x400cc.jpg",
+          caption: "River Flows In You",
+        },
+      ],
+      tabname: "Song",
+      metadata: [
+        {
+          title: "Album",
+          text: "The Best - Reminiscent 10th Anniversary",
+        },
+        {
+          title: "Label",
+          text: "SME Korea Inc.",
+        },
+        {
+          title: "Released",
+          text: "2011",
+        },
+      ],
+    },
+    {
+      type: "VIDEO",
+      tabname: "Video",
+      youtubeurl: {
+        caption: "Yiruma, (이루마) - River Flows in You",
+        image: {
+          dimensions: {
+            width: 1280,
+            height: 720,
+          },
+          url: "https://i.ytimg.com/vi/7maJOI3QMu0/maxresdefault.jpg",
+        },
+        actions: [
+          {
+            name: "video:youtube",
+            type: "webview",
+            share: {
+              subject: "River Flows In You - Yiruma",
+              text: "I used Shazam to discover River Flows In You by Yiruma.",
+              href: "https://www.shazam.com/track/40333609/river-flows-in-you",
+              image:
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/b5/ae/26/b5ae2651-542a-9ee9-be4c-278791e2918f/mzi.ncrxyndi.jpg/400x400cc.jpg",
+              twitter:
+                "I used @Shazam to discover River Flows In You by Yiruma.",
+              html: "https://www.shazam.com/snippets/email-share/40333609?lang=-&country=US",
+              avatar:
+                "https://is4-ssl.mzstatic.com/image/thumb/Features115/v4/3f/84/4e/3f844e97-8c23-b64d-87f1-d536e99f80e9/mzl.rsxdpqqq.jpg/800x800cc.jpg",
+              snapchat: "https://www.shazam.com/partner/sc/track/40333609",
+            },
+            uri: "https://youtu.be/7maJOI3QMu0?autoplay=1",
+          },
+        ],
+      },
+    },
+    {
+      type: "ARTIST",
+      avatar:
+        "https://is4-ssl.mzstatic.com/image/thumb/Features115/v4/3f/84/4e/3f844e97-8c23-b64d-87f1-d536e99f80e9/mzl.rsxdpqqq.jpg/800x800cc.jpg",
+      id: "40008598",
+      name: "Yiruma",
+      verified: false,
+      actions: [
+        {
+          type: "artistposts",
+          id: "40008598",
+        },
+        {
+          type: "artist",
+          id: "40008598",
+        },
+      ],
+      tabname: "Artist",
+    },
+    {
+      type: "RELATED",
+      tabname: "Related",
+    },
+  ],
+};
 
-  try {
-    const response = await fetch(options);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function fetchData() {
+//   const url =
+//     "https://shazam.p.rapidapi.com/shazam-events/list?artistId=73406786&l=en-US&from=2022-12-31&limit=50&offset=0";
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": "3a3651c1a3mshe0ed4971da27f71p152dfajsnfe25950f3559",
+//       "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+//     },
+//   };
 
-fetchData();
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.text();
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// fetchData();
+
+
+const sections = data.sections
+// console.log(sections)
+
+const list = document.getElementById('concert-list')
+const lis = sections.map((item) => {
+  
+  const li = document.createElement('li')
+  const p = document.createElement('p')
+  p.innerText = item.type
+  li.appendChild(p)
+  list.appendChild(li)
+})
+
